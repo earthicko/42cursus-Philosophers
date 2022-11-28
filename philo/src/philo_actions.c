59de2363d25ff2f0eb951f9a57a516344d69b9e2
Donghyeon Lee <earthicko@gmail.com>
2022-11-28 20:22:13 +0900

    count eatings after finish eating

--- philo/src/philo_actions.c
+++ philo/src/philo_actions.c
@@ -19,10 +19,10 @@ void	philo_eat(t_philoinfo *info)
 {
 	philo_push_msg(info, EATING);
 	(info->tableinfo->philo_t_last_eat)[info->i] = (info->buf).t;
-	(info->tableinfo->philo_n_eats)[info->i]++;
 	ft_usleep(info->tableinfo->time_eat, info->tableinfo->n_philos / 2);
 	pthread_mutex_unlock(info->forks[0]);
 	pthread_mutex_unlock(info->forks[1]);
+	(info->tableinfo->philo_n_eats)[info->i]++;
 }
 
 void	philo_sleep(t_philoinfo *info)
