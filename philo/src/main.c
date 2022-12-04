19adeea9d366476f944f8b255f87bcf15426ab77
Donghyeon Lee <earthicko@gmail.com>
2022-12-04 12:15:14 +0900

    prevent data race from status check

--- philo/src/main.c
+++ philo/src/main.c
@@ -30,11 +30,15 @@ int	handle_error(char *error_msg)
 int	check_if_all_eat(t_table *table, t_env *env)
 {
 	int	i;
+	int	n_eat;
 
 	i = 0;
 	while (i < table->n_philos)
 	{
-		if ((table->philo_n_eats)[i] < env->n_eats_until_done)
+		pthread_mutex_lock(table->lock_infos + i);
+		n_eat = (table->philo_n_eats)[i];
+		pthread_mutex_unlock(table->lock_infos + i);
+		if (n_eat < env->n_eats_until_done)
 			return (0);
 		i++;
 	}
@@ -47,15 +51,19 @@ int	check_if_dead(t_table *table, t_env *env)
 	time_t	death_us;
 	time_t	now_us;
 	time_t	elapsed_ms;
+	time_t	last_eat;
 
 	i = 0;
 	while (i < table->n_philos)
 	{
 		now_us = get_t_simulation(env);
-		elapsed_ms = (now_us - (table->philo_t_last_eat)[i]) / 1000;
+		pthread_mutex_lock(table->lock_infos + i);
+		last_eat = (table->philo_t_last_eat)[i];
+		pthread_mutex_unlock(table->lock_infos + i);
+		elapsed_ms = (now_us - last_eat) / 1000;
 		if (elapsed_ms * 1000 > env->time_die)
 		{
-			death_us = (table->philo_t_last_eat)[i] + env->time_die;
+			death_us = last_eat + env->time_die;
 			printf("%ld %d died\n", death_us / 1000, i + 1);
 			return (1);
 		}
